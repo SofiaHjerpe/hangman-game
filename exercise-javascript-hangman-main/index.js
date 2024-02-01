9181c124addf08ec39a3a711f92f16154044e075
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-02-01 12:18:05 +0100

    add logic for playing one game, making new array of secretWord after chosing it

--- exercise-javascript-hangman-main/index.js
+++ exercise-javascript-hangman-main/index.js
@@ -1,72 +1,76 @@
-const words = ["citronjuice", "marsipan", "mammut", "maskros", "kossa", "lövblåsare", "nyponbuske"];
-let gamePlay = false;
-let wrongGuessCount = 0;
+const button = document.querySelector(".start-btn");
+const usedLetters = document.querySelector(".used-letters");
+const letters = document.querySelector(".letters");
+const letterContainer = document.querySelector(".letterContainer");
+const letter = document.querySelector(".letter");
 const head = document.getElementById("head");
-const body = document.getElementById("body");
 const arms = document.getElementById("arms");
 const ground = document.getElementById("ground");
 const legs = document.getElementById("legs");
 const scaffold = document.getElementById("scaffold");
-const imgParts = [];
-imgParts.push(head, arms, legs, ground, body, scaffold);
-//Make an array of each word in the array
-const spliceWord = words.map((word) => word.replace(/ /g, "").split(""));
+const overlay = document.querySelector(".overlay");
+
+const imgParts = [head, arms, legs, ground, body, scaffold];
+const words = ["citronjuice", "marsipan", "mammut", "maskros", "ko", "lövblåsare", "nyponbuske"];
+
+let wrongGuessCount = 0;
+let correctGuessCount = 0;
 
 //Chose a random word
-function getRandomWord(spliceWord) {
-  const random = Math.floor(Math.random() * spliceWord.length);
-  let i = random;
-  return spliceWord[i];
+function getRandomWordFromArray(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
 // define word array
-function addRandomWordToDom(spliceWord) {
-  spliceWord.forEach((letter) => {
-    const newWordString = wordsToHtml(letter, letter.length - 1);
-    let letters = document.querySelector(".letters");
-    letters.insertAdjacentHTML("beforeend", newWordString);
-  });
+function addSecretWordToDom(secretWordArray) {
+  const secretWordHtml = secretWordArray
+    .map(
+      (letter) => `
+    <div class="letterContainer">
+        <p class="letter">${letter}</p>
+    </div>
+    `
+    )
+    .join("");
+
+  letters.innerHTML = secretWordHtml;
 }
 
-const random = Math.floor(Math.random() * spliceWord.length);
-let i = random;
-const button = document.querySelector(".start-btn");
 button.addEventListener("click", (e) => {
   e.target.classList.add("hideButton");
-  gamePlay = true;
-  while (gamePlay) {
-    getRandomWord(i);
-    console.log(spliceWord[i]);
-    addRandomWordToDom(spliceWord);
-    window.addEventListener("keydown", letterCheck);
-    break;
-  }
+  const secretWord = getRandomWordFromArray(words);
+  const secretWordArray = secretWord.split("");
+  console.log(secretWordArray);
+  addSecretWordToDom(secretWordArray);
+  window.addEventListener("keydown", letterCheck);
 });
-function wordsToHtml(spliceWord, index) {
-  return `
-    <div class="letterContainer">
-        <p class="letter"> ${spliceWord[index]}</p>
-    </div>
-    `;
-}
 
 function letterCheck(e) {
   console.log(e.key);
-  const keys = document.querySelector(".keys");
-  keys.insertAdjacentText("beforebegin", e.key);
+  usedLetters.insertAdjacentText("beforeend", e.key);
   const secretWord = Array.from(document.querySelectorAll(".letter"));
   let isGuessCorrect = false;
+
   secretWord.forEach((secretLetter) => {
     console.log(secretLetter.textContent);
     console.log(e.key);
-    if (secretLetter.textContent.trim() === e.key) {
+    if (secretLetter.textContent === e.key) {
       secretLetter.style.display = "block";
       isGuessCorrect = true;
+      correctGuessCount++;
     }
   });
   if (!isGuessCorrect) {
     wrongGuessCount++;
     updatePictureAfterGuess(wrongGuessCount);
+  }
+
+  if (secretWord.length === correctGuessCount) {
+    window.alert("You won the game!");
+    button.classList.remove("hideButton");
+    button.innerText = "Try Again";
+    
   }
 }
 
@@ -74,20 +78,20 @@ function updatePictureAfterGuess(wrongGuessCount) {
   if (wrongGuessCount === 1) {
     imgParts[3].style.fillOpacity = "1";
   }
-  if (wrongGuessCount === 3) {
+  if (wrongGuessCount === 2) {
     imgParts[0].style.fill = "black";
+  }
+  if (wrongGuessCount === 3) {
+    imgParts[5].style.fill = "black";
+  }
+  if (wrongGuessCount === 4) {
+    imgParts[2].style.fill = "black";
   }
   if (wrongGuessCount === 5) {
     imgParts[1].style.fill = "black";
   }
-  if (wrongGuessCount === 7) {
-    imgParts[2].style.fill = "black";
-  }
-  if (wrongGuessCount === 9) {
+  if (wrongGuessCount === 6) {
     imgParts[4].style.fill = "black";
-  }
-  if (wrongGuessCount === 10) {
-    imgParts[5].style.fill = "black";
+    window.alert("Game over :(");
   }
 }
-
